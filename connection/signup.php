078c554da4558b131ad3664ee116d86f337bbd71
Franchette Camoro <nicolecamoro@gmail.com>
2016-06-27 14:55:24 +0800

    modified minor typos

--- connection/signup.php
+++ connection/signup.php
@@ -2,33 +2,20 @@
 
   $data = json_decode(file_get_contents("php://input"));
 
-  $firstname = $mysqli -> real_escape_string($data->firstname);
-  $lastname = $mysqli -> real_escape_string($data->lastname);
-  // $email = $mysqli -> real_escape_string($data->email);
-  $Gend = $mysqli -> real_escape_string($data->Gend);
-  $scrtQuestion1 = $mysqli -> real_escape_string($data->scrtQuestion1);
-  $scrtans1 = $mysqli -> real_escape_string($data->scrtans1);
-  $scrtQuestion2 = $mysqli -> real_escape_string($data->scrtQuestion2);
-  $scrtans2 = $mysqli -> real_escape_string($data->scrtans2);
-  $utype = $mysqli -> real_escape_string($data->utype);
-  $username = $mysqli -> real_escape_string($data->username);
-  $password = $mysqli -> real_escape_string($data->password);
-  // $ConfPas                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         s = $mysqli -> real_escape_string($data->ConfPass);
+  $firstname =  mysql_real_escape_string($data->firstname);
+  $lastname =  mysql_real_escape_string($data->lastname);
+  // $email =  mysql_real_escape_string($data->email);
+  $Gend =  mysql_real_escape_string($data->Gend);
+  $scrtQuestion1 =  mysql_real_escape_string($data->scrtQuestion1);
+  $scrtans1 =  mysql_real_escape_string($data->scrtans1);
+  $scrtQuestion2 =  mysql_real_escape_string($data->scrtQuestion2);
+  $scrtans2 =  mysql_real_escape_string($data->scrtans2);
+  $utype =  mysql_real_escape_string($data->utype);
+  $username =  mysql_real_escape_string($data->username);
+  $password =  mysql_real_escape_string($data->password);
+  // $ConfPass =  mysql_real_escape_string($data->ConfPass);
 
   include 'db.php';
 
-  $query = "INSERT INTO students('FirstName', 'LastName', 'User_type', 'gender')
-    VALUES(?,?,?,?)";
-  $statement = $mysqli->prepare($query);
-
-  $statement->bind_param(
-    '$firstname',
-    '$lastname',
-    '$utype',
-    '$Gend'
-  );
-
-  return true;
-
-
+  
 ?>

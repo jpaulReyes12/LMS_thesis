816bb4aaf82995c62f6fc2e242bf9bce37b88a79
Franchette Camoro <nicolecamoro@gmail.com>
2016-09-27 11:31:22 +0800

    Fixed insert quiz

--- js/services/QuestionService.js
+++ js/services/QuestionService.js
@@ -1,16 +1,30 @@
 angular.module('lmsApp')
 
-.factory('Questions',['$firebaseArray', function($firebaseArray){
+.factory('Questions',['$firebaseArray', "$firebaseObject", function($firebaseArray){
 
-  var ref = firebase.database().ref('/quiz/questions');
-  var questionList = $firebaseArray(ref);
+  var ref = firebase.database().ref('/quiz/');
+  var key = "";
 
-  function addQuiz(q){
-    questionList.$add(q);
+  function setKey(q_key) {
+    key = q_key;
+    console.log(key);
+  }
+
+  function getKey() {
+    console.log(key);
+    return key;
+  }
+
+  function addQuiz(q, parentKey){
+    var ref = firebase.database().ref('/quiz/' + parentKey + "/questions");
+    var questionList = $firebaseArray(ref);
+    return questionList.$save(q).key;
   }
 
   return{
-    addQuiz: addQuiz
+    addQuiz: addQuiz,
+    setKey: setKey,
+    getKey: getKey
   }
 
 }])

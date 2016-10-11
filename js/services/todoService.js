c5ee3dbbf429f74bef2e1ef23c5b81cf504983bd
Franchette Camoro <nicolecamoro@gmail.com>
2016-10-11 10:11:43 +0800

    todo fixes

--- js/services/todoService.js
+++ js/services/todoService.js
@@ -4,15 +4,15 @@ angular.module('lmsApp')
 .factory('Todo', [ '$firebaseArray', function ($firebaseArray) {
 
   var uid;
+  var ref = firebase.database().ref('users/' + uid + '/todos');
 
-  function getTodos() {
-    var ref = firebase.database().ref('users/' + uid + '/todos');
-    var todoList = $firebaseArray(ref);
-    return todoList;
+  function getTodos(IDuser) {
+    var ref = firebase.database().ref('users/' + IDuser + '/todos');
+    var todoLists = $firebaseArray(ref);
+    return todoLists;
   }
 
   function addTodo(data) {
-    var ref = firebase.database().ref('users/' + uid + '/todos');
     var todoList = $firebaseArray(ref);
     todoList.$add(data);
   }

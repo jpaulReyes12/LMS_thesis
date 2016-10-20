a737106177a569c8f1ec24fcbaa6f792ca148029
Anton <antonio_amolo@ymail.com>
2016-10-20 14:08:59 +0800

    change class record add student

--- js/controllers/teacher/classRecordCtrl.js
+++ js/controllers/teacher/classRecordCtrl.js
@@ -3,59 +3,63 @@ angular.module('lmsApp')
 
     $scope.classID = $routeParams.id;
 
-    var ref = firebase.database().ref('/users');
+    $scope.users = Users.getUsers();
 
-    (function() {
-      $firebaseArray(ref).$loaded().then(function(c) {
-        $scope.users = c;
-      }).catch(function(err) {
-        return err;
-      });
+    //
+    //
+    // var ref = firebase.database().ref('/users');
+    //
+    // (function() {
+    //   console.log("loading");
+    //   $firebaseArray(ref).$loaded().then(function(c) {
+    //     $scope.users = c;
+    //   }).catch(function(err) {
+    //     return err;
+    //   });
+    //
+    // })()
 
-    })()
+    // $scope.add = function(val) {
+    //   ref.on('child_added', function(snap) {
+    //       if (snap.val().email === val) {
+    //         getid(snap.key, snap.val());
+    //
+    //       }
+    //   });
+    //
+    //   $scope.selectStudents = "";
+    // }
+    //
+    // function getid(id, info) {
+    //
+    //   var parent = Class.getClass();
+    //   var parent_class = parent[$routeParams.id];
+    //   var classList = $firebaseArray(ref.child(id).child('classes'));
+    //
+    //
+    //   classList.$add({
+    //     subject: parent_class.$id,
+    //     subject_name: parent_class.Subject,
+    //   });
+    //
+    //
+    //   Class.addStudent($scope.classID, {
+    //     fname: info.firstname,
+    //     lname: info.lastname,
+    //     email: info .email,
+    //     s_id: info.uid
+    //   });
+    //
+    // }
+    //
+    // $scope.students = function functionName() {
+    //   var classes =  Class.getClass();
+    //   return classes[$routeParams.id];
+    // };
+    //
+    // $scope.removeStudent = function(id){
+    //
+    //   Class.removeStudent(id, $routeParams.id);
+    // }
 
-
-    $scope.add = function(val) {
-      ref.on('child_added', function(snap) {
-          if (snap.val().email === val) {
-            getid(snap.key, snap.val());
-
-          }
-      });
-
-      $scope.selectStudents = "";
-    }
-
-    function getid(id, info) {
-
-      var parent = Class.getClass();
-      var parent_class = parent[$routeParams.id];
-      var classList = $firebaseArray(ref.child(id).child('classes'));
-
-
-      classList.$add({
-        subject: parent_class.$id,
-        subject_name: parent_class.Subject,
-      });
-
-
-      Class.addStudent($scope.classID, {
-        fname: info.firstname,
-        lname: info.lastname,
-        email: info .email,
-        s_id: info.uid
-      });
-
-    }
-
-    $scope.students = function functionName() {
-      var classes =  Class.getClass();
-      return classes[$routeParams.id];
-    };
-
-    $scope.removeStudent = function(id){
-
-      Class.removeStudent(id, $routeParams.id);
-    }
-
-}])
+}]);

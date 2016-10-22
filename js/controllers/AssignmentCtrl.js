937b0e75c462bd02bfb8436fe7cd73f646d2c166
Anton <antonio_amolo@ymail.com>
2016-10-22 22:16:02 +0800

    createvalidation

--- js/controllers/AssignmentCtrl.js
+++ js/controllers/AssignmentCtrl.js
@@ -4,11 +4,14 @@ angular.module('lmsApp')
   var id = $routeParams.id
 
   $scope.saveAss = function(a, id){
-
+    a.deadline = Math.floor(a.deadline/1000);
     a.timecreated = Math.floor(Date.now()/1000);
     Assignment.addAss(a);
     $location.path('/teacher/assignment');
   }
+
+  var datetime = new Date();
+  $scope.dateNow = datetime;
 
   $scope.classID = $routeParams.id;
 }])
